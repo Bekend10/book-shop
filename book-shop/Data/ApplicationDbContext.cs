3a13558c98d594e76e16b8528eb437b379419fb1
Bekend10 <tiendungk18a1@gmail.com>
2025-04-29 00:21:40 +0700

    update database

--- book-shop/Data/ApplicationDbContext.cs
+++ book-shop/Data/ApplicationDbContext.cs
@@ -12,12 +12,18 @@ namespace book_shop.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Role> Roles { get; set; }
+        public DbSet<Address> Address { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>()
                 .HasOne(u => u.Account)
                 .WithOne(a => a.user)
                 .HasForeignKey<Account>(a => a.user_id);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Address)
+                .WithOne(a => a.User)
+                .HasForeignKey<Address>(a => a.address_id);
 
             modelBuilder.Entity<Account>()
                 .HasOne(a => a.role)
