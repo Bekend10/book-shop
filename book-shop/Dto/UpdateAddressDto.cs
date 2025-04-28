2642d5739fa26596d3cfc6d4b89b22090e4124cf
Bekend10 <tiendungk18a1@gmail.com>
2025-04-29 00:21:17 +0700

    init address model and dto

---
+++ book-shop/Dto/UpdateAddressDto.cs
@@ -0,0 +1,11 @@
+﻿namespace book_shop.Dto
+{
+    public class UpdateAddressDto
+    {
+        public string country { get; set; } = string.Empty;
+        public string councious { get; set; } = string.Empty ;
+        public string district { get; set; } = string .Empty ;
+        public string commune { get; set; } = string.Empty;
+        public string house_number { get; set; } = string.Empty;
+    }
+}
--- book-shop/Models/User.cs
+++ book-shop/Models/User.cs
@@ -15,5 +15,6 @@ namespace book_shop.Models
         public bool gender { get; set; }
         public DateTime dob { get; set; }
         public Account Account { get; set; }
+        public Address Address { get; set; }
     }
 }
