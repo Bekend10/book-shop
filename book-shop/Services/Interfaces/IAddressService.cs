9e44e620ce31dce3297e195c4899f50a1d30f79c
Bekend10 <tiendungk18a1@gmail.com>
2025-04-29 00:20:41 +0700

    init address service

---
+++ book-shop/Services/Interfaces/IAddressService.cs
@@ -0,0 +1,13 @@
+﻿using book_shop.Dto;
+using book_shop.Models;
+
+namespace book_shop.Services.Interfaces
+{
+    public interface IAddressService
+    {
+        Task<object> GetAllAddresses();
+        Task<object> GetAddressById(int id);
+        Task<object> UpdateAddress(int id, UpdateAddressDto address);
+        Task<object> DeleteAddress(int id);
+    }
+}
