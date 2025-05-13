ae3d67f7a64ec5b02603ae107f5076a943651354
Bekend10 <tiendungk18a1@gmail.com>
2025-05-14 00:09:23 +0700

    add helper to take current userid

---
+++ book-shop/Helpers/UserHelper/UserHelper.cs
@@ -0,0 +1,34 @@
+﻿using book_shop.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace book_shop.Helpers.UserHelper
+{
+    public class UserHelper
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserHelper(ApplicationDbContext dbContext, IHttpContextAccessor httpContextAccessor)
+        {
+            _dbContext = dbContext;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public int GetCurrentUserId()
+        {
+            var userClaims = _httpContextAccessor.HttpContext?.User;
+
+            if (userClaims == null)
+                return -1;
+
+            var userIdStr = userClaims.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (!int.TryParse(userIdStr, out int userId))
+                return -1;
+
+            return userId;
+        }
+    }
+}
