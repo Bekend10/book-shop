8ad367e85d728368afa24ca6a79ae8d9dd4c591d
Bekend10 <tiendungk18a1@gmail.com>
2025-05-14 00:08:23 +0700

    add category repository

---
+++ book-shop/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using book_shop.Models;
+
+namespace book_shop.Repositories.Interfaces
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        Task<Category> GetCategoryByNameAsync(string name);
+        Task<IEnumerable<Category>> GetCategoriesByBookIdAsync(int bookId);
+        Task<IEnumerable<Category>> GetCategoriesByBookIdsAsync(IEnumerable<int> bookIds);
+        Task<IEnumerable<Category>> GetCategoriesByBookIdAndCategoryIdAsync(int bookId, int categoryId);
+        Task<IEnumerable<Category>> GetCategoriesByBookIdAndCategoryIdsAsync(int bookId, IEnumerable<int> categoryIds);
+    }
+}
