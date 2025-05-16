f8bf224f996d1c72d88722fc9c5ad1c8ee456437
Bekend10 <tiendungk18a1@gmail.com>
2025-05-16 23:50:53 +0700

    create cloud service

---
+++ cloudinary-service/Services/ICloudinaryService.cs
@@ -0,0 +1,11 @@
+﻿using cloudinary_service.Models;
+using Microsoft.Extensions.Options;
+
+namespace cloudinary_service.Services
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadImageAsync(IFormFile image);
+        Task<bool> DeleteImageAsync(string publicId);
+    }
+}
