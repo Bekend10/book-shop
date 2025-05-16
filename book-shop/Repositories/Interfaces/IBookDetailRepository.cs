8f44f38a2f45c61e2d68a8da763a73c196afe5c7
Bekend10 <tiendungk18a1@gmail.com>
2025-05-16 23:46:47 +0700

    create interface repository for book

---
+++ book-shop/Repositories/Interfaces/IBookDetailRepository.cs
@@ -0,0 +1,9 @@
+﻿using book_shop.Models;
+
+namespace book_shop.Repositories.Interfaces
+{
+    public interface IBookDetailRepository : IRepository<BookDetail>
+    {
+        Task<BookDetail> GetBookDetailsByBookIdAsync(int bookId);
+    }
+}
---
+++ book-shop/Repositories/Interfaces/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using book_shop.Models;
+
+namespace book_shop.Repositories.Interfaces
+{
+    public interface IBookRepository : IRepository<Book>
+    {
+        Task<IEnumerable<Book>> GetBooksByAuthorIdAsync(int authorId);
+        Task<IEnumerable<Book>> GetBooksByCategoryIdAsync(int categoryId);
+        Task<IEnumerable<Book>> GetBooksByPublisherAsync(string publisher);
+        Task<IEnumerable<Book>> GetBooksByTitleAsync(string title);
+    }
+}
