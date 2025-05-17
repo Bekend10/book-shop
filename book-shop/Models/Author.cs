ed1a09271f75aba4ee6692ec0a02e95065c1757a
Bekend10 <tiendungk18a1@gmail.com>
2025-05-17 14:32:16 +0700

    create model author and update attribute authorId for relationship between author and book

---
+++ book-shop/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace book_shop.Models
+{
+    public class Author
+    {
+        [Key]
+        public int author_id { get; set; }
+        public string name  { get; set; } = string.Empty;
+        public string bio { get; set; } = string.Empty;
+        public DateTime dob { get; set; }
+        public string nationally { get; set; } = string.Empty;
+        public ICollection<Book> books { get; set; }
+    }
+}
