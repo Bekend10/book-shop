f6433d4a4cd24f3e2adfeaf3cfc5b205928d3b87
Bekend10 <tiendungk18a1@gmail.com>
2025-07-05 16:57:23 +0700

    Feature/scrum-1 : Add information about user when login

--- book-shop/Dto/UserDto.cs
+++ book-shop/Dto/UserDto.cs
@@ -7,4 +7,15 @@
         public string last_name { get; set; } = string.Empty;
         public string email { get; set; } = string.Empty;
     }
+
+    public class UserRespone
+    {
+        public int user_id { get; set; }
+        public string? first_name { get; set; } = string.Empty;
+        public string? last_name { get; set; } = string.Empty;
+        public string? email { get; set; } = string.Empty;
+        public string? profile_image { get; set; } = string.Empty;
+        public string? full_name { get; set; } = string.Empty;
+        public string? role { get; set; }
+    }
 }
