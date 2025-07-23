6ce78fb9779237260009b78a057eb62963c57144
Bekend10 <tiendungk18a1@gmail.com>
2025-07-23 14:06:26 +0700

    feature/scrum-27 : Allow user login with google account

---
+++ book-shop/Migrations/20250722082413_AddColumnGoogleIDForAccountTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace book_shop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnGoogleIDForAccountTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "google_id",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "google_id",
+                table: "Users");
+        }
+    }
+}
